cca3d96339f694169a9c153a1e6d9bb6dfebc578
Umbigo Preto <umbigopreto@outlook.com.br>
2026-08-11 21:25:50 -0300

    fix(VisceralCombat): increase limbSize scale from 0.001f to 0.01f to satisfy Unity C++ float LookRotation precision threshold

--- mods/VisceralCombat/modded/VisceralCombat/VisceralCombat.Ragdolls.Classes/RagdollHelperClass.cs
+++ mods/VisceralCombat/modded/VisceralCombat/VisceralCombat.Ragdolls.Classes/RagdollHelperClass.cs
@@ -27,7 +27,7 @@ public static class RagdollHelperClass
 
 	private static float Anim_Stomach2_Length = 7f;
 
-	internal static Vector3 limbSize = new Vector3(0.001f, 0.001f, 0.001f);
+	internal static Vector3 limbSize = new Vector3(0.01f, 0.01f, 0.01f);
 
 	/// <summary>
 	/// Zeroes the muscle weight of muscles belonging to a dismembered limb so that
